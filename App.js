0a8ac61918237af243a949530449fc69365b0f26
LovishGoyal01 <goyallovish852@gmail.com>
2025-06-27 01:12:11 +0530

    Updated something

--- App.js
+++ App.js
@@ -1,9 +1,810 @@
 import React from "react"; 
 import ReactDOM from "react-dom/client"; 
- console.log("React script is running");
- 
- const heading= React.createElement("h1",{ id:"heading" },"This is my first react");
 
- const root=ReactDOM.createRoot(document.getElementById("root"));
+const Header = () => {
+    return(
+        <div className="header">
+           <div className="logo-container">
+              <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?industry=COMPANY+NAME"></img>
+           </div>
+           <div className="nav-items">
+             <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+             </ul>
+           </div>
+        </div>
+    )
+}
 
- root.render(heading);
+const RestaurantCard = (props) => {
+
+      const {resdata}=props;
+      const {cloudinaryImageId,name,cuisines,avgRating,costForTwo,deliveryTime}=resdata?.data;
+    return(
+    <div className="res-card">
+        <img className="res-logo"
+         alt="res-logo"
+         src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId}
+        />
+        <h3>{name}</h3>
+        <h4>{cuisines.join(", ")}</h4>
+        <h4>{avgRating}</h4>
+        <h4>{costForTwo}</h4>
+        <h4>{deliveryTime}</h4>
+    </div>
+    ) 
+}
+
+const reslist=[
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"data": {
+"id": "939816",
+"name": "Chinese Wok",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/8/11/49552e97-87f9-44ad-ab81-c6b86d83ef91_939816.jpg",
+"locality": "Bathinda City(Model Town)",
+"areaName": "Bathinda City(Model Town)",
+"costForTwo": "₹200 for two",
+"cuisines": [
+"Chinese",
+"Beverages"
+],
+"avgRating": 3.5,
+"parentId": "61955",
+"avgRatingString": "3.5",
+"totalRatingsString": "71",
+"deliveryTime": 22,
+"sla": {
+"lastMileTravel": 2.1,
+"serviceability": "SERVICEABLE",
+"slaString": "20-25 mins",
+"lastMileTravelString": "2.1 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-06-27 02:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "bolt/big%20rx%20listing%2015%20mins%20.png",
+"description": "bolt!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "bolt!",
+"imageId": "bolt/big%20rx%20listing%2015%20mins%20.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "70% OFF",
+"discountTag": "FLAT DEAL"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-ca729467-6d61-4046-9831-e10de2df031c"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/bathinda/chinese-wok-bathinda-city-model-town--rest939816",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"data": {
+"id": "168027",
+"name": "KFC",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/4f1aaf19-7eaa-4271-9d00-5ab3f6acfec1_168027.JPG",
+"locality": "Bathinda Road",
+"areaName": "Bibi Wala Road",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Burgers",
+"Fast Food",
+"Rolls & Wraps"
+],
+"avgRating": 4.3,
+"parentId": "547",
+"avgRatingString": "4.3",
+"totalRatingsString": "4.3K+",
+"deliveryTime": 14,
+"sla": {
+"lastMileTravel": 1.3,
+"serviceability": "SERVICEABLE",
+"slaString": "10-15 mins",
+"lastMileTravelString": "1.3 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-03 00:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "bolt/big%20rx%20listing%2015%20mins%20.png",
+"description": "bolt!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "bolt!",
+"imageId": "bolt/big%20rx%20listing%2015%20mins%20.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹59"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-ca729467-6d61-4046-9831-e10de2df031c"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/bathinda/kfc-road-bibi-wala-road-rest168027",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"data": {
+"id": "1054015",
+"name": "Honest Bowl",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/2/27/810f4741-9fa9-4dbd-88ce-c50311ad1903_1054015.JPG",
+"locality": "GT Road",
+"areaName": "Near Namdev Gate Main",
+"costForTwo": "₹200 for two",
+"cuisines": [
+"Kebabs",
+"Fast Food",
+"Snacks",
+"American",
+"Healthy Food",
+"Desserts",
+"Beverages"
+],
+"avgRating": 4.1,
+"parentId": "612552",
+"avgRatingString": "4.1",
+"totalRatingsString": "18",
+"deliveryTime": 19,
+"sla": {
+"lastMileTravel": 1.5,
+"serviceability": "SERVICEABLE",
+"slaString": "15-20 mins",
+"lastMileTravelString": "1.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-06-27 03:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "bolt/big%20rx%20listing%2015%20mins%20.png",
+"description": "bolt!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "bolt!",
+"imageId": "bolt/big%20rx%20listing%2015%20mins%20.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹99"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"isNewlyOnboarded": true,
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-ca729467-6d61-4046-9831-e10de2df031c"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/bathinda/honest-bowl-gt-road-near-namdev-gate-main-rest1054015",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"data": {
+"id": "1054359",
+"name": "Dabba & Co.",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/3/19/64dddf23-2af9-419c-8888-9fb6b10bcbbd_1054359.jpg",
+"locality": "GT Road",
+"areaName": "Near Namdev Gate Main",
+"costForTwo": "₹200 for two",
+"cuisines": [
+"Home food",
+"Indian",
+"Thalis"
+],
+"avgRating": 3.8,
+"parentId": "612560",
+"avgRatingString": "3.8",
+"totalRatingsString": "13",
+"deliveryTime": 13,
+"sla": {
+"lastMileTravel": 1.5,
+"serviceability": "SERVICEABLE",
+"slaString": "10-15 mins",
+"lastMileTravelString": "1.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-06-27 03:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "bolt/big%20rx%20listing%2015%20mins%20.png",
+"description": "bolt!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "bolt!",
+"imageId": "bolt/big%20rx%20listing%2015%20mins%20.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹89"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"isNewlyOnboarded": true,
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-ca729467-6d61-4046-9831-e10de2df031c"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/bathinda/dabba-and-co-gt-road-near-namdev-gate-main-rest1054359",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"data": {
+"id": "1030993",
+"name": "Thalaiva Biryani",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/21/e69a6a31-f530-4424-9aeb-031eb87271ac_1030993.JPG",
+"locality": "GT Road",
+"areaName": "Near Namdev Gate Main",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Biryani",
+"Mughlai",
+"Lucknowi",
+"Hyderabadi",
+"Kebabs",
+"Desserts",
+"Beverages"
+],
+"avgRating": 4.3,
+"parentId": "582792",
+"avgRatingString": "4.3",
+"totalRatingsString": "12",
+"deliveryTime": 18,
+"sla": {
+"lastMileTravel": 1.5,
+"serviceability": "SERVICEABLE",
+"slaString": "15-20 mins",
+"lastMileTravelString": "1.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-06-27 03:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "bolt/big%20rx%20listing%2015%20mins%20.png",
+"description": "bolt!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "bolt!",
+"imageId": "bolt/big%20rx%20listing%2015%20mins%20.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹89"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"isNewlyOnboarded": true,
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-ca729467-6d61-4046-9831-e10de2df031c"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/bathinda/thalaiva-biryani-gt-road-near-namdev-gate-main-rest1030993",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"data": {
+"id": "1011613",
+"name": "Faasos Signature Wraps & Rolls",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/19/4b7b4a0c-5f48-49af-ab0d-94fbfc208472_1011613.jpg",
+"locality": "GT Road",
+"areaName": "Near Namdev Gate Main",
+"costForTwo": "₹350 for two",
+"cuisines": [
+"Fast Food",
+"Snacks",
+"Desserts",
+"Beverages"
+],
+"avgRating": 4.2,
+"parentId": "340366",
+"avgRatingString": "4.2",
+"totalRatingsString": "64",
+"deliveryTime": 15,
+"sla": {
+"lastMileTravel": 1.5,
+"serviceability": "SERVICEABLE",
+"slaString": "15-20 mins",
+"lastMileTravelString": "1.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-06-27 03:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹129"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"isNewlyOnboarded": true,
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-ca729467-6d61-4046-9831-e10de2df031c"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/bathinda/faasos-signature-wraps-and-rolls-gt-road-near-namdev-gate-main-rest1011613",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"data": {
+"id": "1011618",
+"name": "The Pizza Project by Oven Story",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/19/0bffadf0-e294-4373-bbe5-72fc9debc4bc_1011618.jpg",
+"locality": "GT Road",
+"areaName": "Near Namdev Gate Main",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Pizzas",
+"Italian",
+"Desserts",
+"Beverages"
+],
+"avgRating": 4,
+"parentId": "497031",
+"avgRatingString": "4.0",
+"totalRatingsString": "17",
+"deliveryTime": 14,
+"sla": {
+"lastMileTravel": 1.5,
+"serviceability": "SERVICEABLE",
+"slaString": "10-15 mins",
+"lastMileTravelString": "1.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-06-27 03:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "bolt/big%20rx%20listing%2015%20mins%20.png",
+"description": "bolt!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "bolt!",
+"imageId": "bolt/big%20rx%20listing%2015%20mins%20.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹89"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"isNewlyOnboarded": true,
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-ca729467-6d61-4046-9831-e10de2df031c"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/bathinda/the-pizza-project-by-oven-story-gt-road-near-namdev-gate-main-rest1011618",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"data": {
+"id": "1011604",
+"name": "Veg Daawat by Behrouz",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/19/87884871-7c21-4aed-bb85-d43fee9a00a2_1011604.jpg",
+"locality": "GT Road",
+"areaName": "Near Namdev Gate Main",
+"costForTwo": "₹700 for two",
+"cuisines": [
+"Biryani",
+"Kebabs",
+"Mughlai",
+"Lucknowi",
+"Hyderabadi",
+"Desserts",
+"Beverages"
+],
+"avgRating": 3.9,
+"veg": true,
+"parentId": "344904",
+"avgRatingString": "3.9",
+"totalRatingsString": "8",
+"deliveryTime": 16,
+"sla": {
+"lastMileTravel": 1.5,
+"serviceability": "SERVICEABLE",
+"slaString": "15-20 mins",
+"lastMileTravelString": "1.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-06-27 03:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹229"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"isNewlyOnboarded": true,
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-ca729467-6d61-4046-9831-e10de2df031c"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/bathinda/veg-daawat-by-behrouz-gt-road-near-namdev-gate-main-rest1011604",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+}
+];
+
+const Body = () => {
+    return(
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+             { reslist.map((restaurant) => (
+                 <RestaurantCard key={restaurant.data.id} resdata={restaurant} />
+             ))}
+             
+            </div>
+        </div>
+    )
+}
+
+
+const AppLayout = () => {
+    return(
+      <div className="app">
+      <Header/>
+      <Body/> 
+      </div>
+    )
+}
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
